bc540ebb338531c5543c638ae23bde258875c77b
Yathaartha <maharjanyathaartha017@gmail.com>
2021-06-30 14:56:58 +0545

    fixed styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="container">
-        <div className="title">Crypto Listing</div>
+        <div className="title">CRYPTO LISTING</div>
       </div>
     </div>
   );
